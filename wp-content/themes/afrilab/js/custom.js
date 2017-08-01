6be73c8df1f4984a6ba8df7c0a697ea885132cab
edgar chris <edgarchris4@gmail.com>
2017-08-01 22:27:50 +0300

    add cf7 to project

--- wp-content/themes/afrilab/js/custom.js
+++ wp-content/themes/afrilab/js/custom.js
@@ -263,36 +263,7 @@ function achivementCarousel() {
 //===Testimonial===
 function testimonialSlide() {
     if ($('.testimonial-carousel').length) {
-        $('.testimonial-carousel').owlCarousel({
-            loop: true,
-            margin: 30,
-            nav: false,
-            dots: true,
-            autoplayHoverPause: false,
-            autoplay: 6000,
-            smartSpeed: 700,
-            navText: ['<span class="fa fa-angle-left"></span>', '<span class="fa fa-angle-right"></span>'],
-            responsive: {
-                0: {
-                    items: 1
-                },
-                600: {
-                    items: 1
-                },
-                800: {
-                    items: 1
-                },
-                1024: {
-                    items: 1
-                },
-                1100: {
-                    items: 1
-                },
-                1200: {
-                    items: 1
-                }
-            }
-        })
+        $('.testimonial-carousel').owlCarousel({})
     }
 }
 
